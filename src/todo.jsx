b86515729b65213782a6d5a7558836c05c767521
Kazi Fabiha Golam Liya <151756377+KaziFabihaLiya@users.noreply.github.com>
2025-09-19 15:52:12 +0600

    conditional rendering end

--- src/todo.jsx
+++ src/todo.jsx
@@ -12,11 +12,46 @@
 //         return <li>Pending: {task}</li>
 //     }
 // }
+// export default function ToDo({task, isDone, time=0}){
+//     if(isDone === true){
+//         return <li>Done: {task} Duration: {time}</li>
+//     }
+//     else{
+//         return <li>Pending: {task} Duration: {time}</li>
+//     }
+// }
+
+// conditional rendering : 3 ternary
+// condition ? true : false
+
+// export default function ToDo({task, isDone, time = 0}){
+
+//     return isDone ? <li>Done: {task}, Time: {time}</li> : <li>Not Done: {task}, Time: {time}</li>
+// }
+
+// conditional rendering : 4 &&
+
+// export default function ToDo({task, isDone, time = 0}){
+
+//     return isDone && <li>Done Tasks: {task} | Time: {time}</li> 
+// }
+// conditional rendering : 5 ||
+
+// export default function ToDo({task, isDone, time = 0}){
+
+//     return isDone || <li>Not Done Tasks: {task} | Time: {time}</li> 
+// }
+
+// conditional rendering : 6 use variable
 export default function ToDo({task, isDone, time=0}){
+    //const displayTime = time ? time : 100;
+    let listItem;
+
     if(isDone === true){
-        return <li>Done: {task} Duration: {time}</li>
+        listItem = <li>Done: {task} Duration: {time ? 'I am done' : 'not done yet'}</li>;
     }
     else{
-        return <li>Pending: {task} Duration: {time}</li>
+        listItem = <li>Pending: {task} </li>
     }
+    return listItem
 }
