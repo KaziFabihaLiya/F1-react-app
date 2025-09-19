70b91fa549c27dbb1e0476cd208fc6ffc7dc50ab
Kazi Fabiha Golam Liya <151756377+KaziFabihaLiya@users.noreply.github.com>
2025-09-19 15:11:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>React Core Concepts</h1>
+      <Student></Student>
+      <Student></Student>
+      <Person></Person>
+      <Sports></Sports>
+      <Developer name="Liya" tech="JS Stack"></Developer>
+      <Developer name="
+      Ahad" tech="MERN Stack"></Developer>
+    </>
+  )
+}
+
+function Developer(props){
+  return(
+    <div style={{
+      border: '2px solid green',
+      borderRadius: '10px'
+    }}>
+      <h3>Developer Name: {props.name} </h3>
+      <p>Technology : {props.tech}</p>
+    </div>
+  )
+}
+function Student(){
+  return(
+    <div className='student'>
+      <p>Name: </p>
+      <p>Department: </p>
+    </div>
+  )
+}
+
+function Person(){
+  const age = 17;
+  const name = "Liya";
+  const personStyle = {
+    color: 'red',
+    border: '2px dotted salmon',
+    borderRadius: '10px',
+    fontSize:'1.5rem'
+  }
+  return (
+    <p style={personStyle}> Hey, It's {name}, I am {age}. Nice to meet you</p>
+  )
+}
+
+function Sports(){
+  return(
+    <div>
+      <h3>Cricket</h3>
+      <p>Playing and losing</p>
+    </div>
+  )
+}
+export default App
